2c498c32dd76a9981c20d62d5b1882888e275bea
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-06 18:57:44 +0900

    [add]createアクション追加

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -3,4 +3,23 @@ class TodolistsController < ApplicationController
 
   	@list = List.new
   end
+
+
+  def create
+
+  	list = List.new(list_params)
+
+  	list.save
+
+  	redirect_to '/top'
+
+  end
+
+  private
+
+  def list_params
+
+  	params.require(:list).permit(:title, :body)
+
+  end
 end
