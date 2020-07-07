8ce1c705bcb1c3c826e8e9d5b5f710c095b3eb16
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-07 14:32:12 +0900

    [Finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c8ab48ce487ddc03e34e3e2c7c7ad951a9be367bbd5aef9b81b41910c7f0c078e1714b653168fa504406c56cd4bf0135013948fbafe3a83e67cce1fe00f3683d'
